f240c2b602a13453793f5f22caee368f5b8ccd2a
Ben <bigriffin94@outlook.com>
2020-06-04 13:33:14 -0700

    steps 1-3 are good reading the file and splitting my words analyzed the text data and created a list of words that can follow particular words choosing a random starting word based off limitations

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,34 @@
 import random
 
-# Read in all the words in one go
+# 1. Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
+    split_words = words.split()
+    # print(split_words, end=" ")
 
 # TODO: analyze which words can follow other words
 # Your code here
+""" 
+POPULAR WORDS in TXT file
+kitten
+the
+little
+sleep
+day
+"""
+
+# build a list for words that can follow other words
+words_that_follow = "kitten the little sleep day"
+split_follow_words = words_that_follow.split()
+# print(split_follow_words)
+
+start_word = random.choice(split_words)
+# print(type(start_word))
+# print(start_word[0])
+if start_word[0].isupper() == True:
+    print(start_word[0])
+# print("START WORD", start_word)
 
 
 # TODO: construct 5 random sentences
 # Your code here
-
