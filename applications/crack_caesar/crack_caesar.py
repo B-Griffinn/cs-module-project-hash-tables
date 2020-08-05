934ef4c29d40efa3ec7948e81816ceafdc07e240
Ben <bigriffin94@outlook.com>
2020-08-05 10:33:29 -0700

    crack_caesar

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,63 @@
 
 # Your code here
 
+# hash table as a map between data values
+
+# GOATS ~~>  JEHFN
+
+encode_table = {
+    'A': 'H',
+    'B': 'Z',
+    'C': 'Y',
+    'D': 'W',
+    'E': 'O',
+    'F': 'R',
+    'G': 'J',
+    'H': 'D',
+    'I': 'P',
+    'J': 'T',
+    'K': 'I',
+    'L': 'G',
+    'M': 'L',
+    'N': 'C',
+    'O': 'E',
+    'P': 'X',
+    'Q': 'K',
+    'R': 'U',
+    'S': 'N',
+    'T': 'F',
+    'U': 'A',
+    'V': 'M',
+    'W': 'B',
+    'X': 'Q',
+    'Y': 'V',
+    'Z': 'S'
+}
+
+
+def encode(s):
+    result = ""
+
+    for i in s:
+        result += encode_table[i]
+
+    return result
+
+
+decode_table = {}
+
+for k, v in encode_table.items():
+    decode_table[v] = k
+
+
+def decode(s):
+    r = ""
+
+    for c in s:
+        r += decode_table[c]
+
+    return r
+
+
+print(encode("GOATS"))
+print(decode("JEHFN"))
